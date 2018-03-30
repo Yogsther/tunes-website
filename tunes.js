a51805d2f71d5101c2054f6c22b31f537406abd7
Olle Kaiser <yogsther@gmail.com>
2018-03-30 02:26:05 +0200

    Added support for unsupported song names, added auto-load more content on scroll, added album song count

--- tunes.js
+++ tunes.js
@@ -17,8 +17,28 @@ socket.on("albums", pack => {
      fillShelf();
  });
 
+ function getSizeOfAlumbs(){
+     songs.forEach(song => {
+         for(let i = 0; i < albums.length; i++){
+             if(albums[i].fullAlbum == song.album){
+                 if(albums[i].size == undefined) albums[i].size = 0;
+                 albums[i].size++;
+             }
+         }
+     })
+ }
+
+ function checkPos(){
+     var shelf = document.getElementById("shelf");
+     var scroll = shelf.scrollTop;
+     if(scroll + shelf.clientHeight == shelf.scrollHeight){
+        fillMore()
+     }
+ }
 
  function fillTable(){
+
+    getSizeOfAlumbs();
 
     albums.sort(function(a, b) {
         var textA = a.shortAlbum.toUpperCase();
@@ -26,19 +46,50 @@ socket.on("albums", pack => {
         return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
     });
     
-    document.getElementById("category-table").innerHTML = '<option value="null" class="table-values">All albums</option>'   
+    document.getElementById("category-table").innerHTML = '<option value="null" class="table-values">All songs ' + "(" + songs.length + ")" + '</option>'   
      albums.forEach(album => {
-        document.getElementById("category-table").innerHTML += '<option value="' + album.shortAlbum + '" class="table-values">' + album.shortAlbum + '</option>'   
+        document.getElementById("category-table").innerHTML += '<option value="' + album.shortAlbum + '" class="table-values">' + album.shortAlbum + " (" + album.size + ")" + '</option>'   
      })
      
  }
 
+ function fillMore(){
+    var tableValue = document.getElementById("category-table").value;
+    var search = document.getElementById("search").value.split(" ");
+    if(goneTo >= songs.length) return;
+    var staffed = 0;
+    var i = goneTo;
+    while(staffed < 50 && i < songs.length){
+        var song = songs[i];
+        var valid = true;
+        for(let i = 0; i < search.length; i++){
+            if(search[i] != false && song.fullName.toLowerCase().indexOf(search[i].toLowerCase()) == -1) valid = false;
+        }
+        var album = getAlbum(song.album)
+        if(tableValue != "null" && tableValue != album.shortAlbum) valid = false;
+        if(valid){
+        songsShown++;
+        displayedSongs[i] = song;
+        var name = song.title;
+        if(name.length > 44) name = name.substr(0, 44) + "...";
+        document.getElementById("shelf").innerHTML+= "<div class='slot'> <img draggable='false' src='" + album.art + "' alt='Cover art' title='" + song.fullName + "' class='cover-art'> <span class='title'>" + name + "</span> <button class='btn queue' onclick='queue(" + i + ")'>Queue</button> <button class='btn play' onclick='play(" + i + ")'>Play</button> </div>";
+        staffed++;
+       }
+       i++;
+   }
+   window.goneTo = i;
+ }
+
+ var displayedSongs = new Array();
+ var songsShown = 0;
  function fillShelf(){
      document.getElementById("shelf").innerHTML = "";
      var tableValue = document.getElementById("category-table").value;
      var search = document.getElementById("search").value.split(" ");
      var i = 0; 
      var staffed = 0;
+     displayedSongs = [];
+     songsShown = 0;
      while(staffed < 50 && i < songs.length){
          var song = songs[i];
          var valid = true;
@@ -48,22 +99,29 @@ socket.on("albums", pack => {
          var album = getAlbum(song.album)
          if(tableValue != "null" && tableValue != album.shortAlbum) valid = false;
          if(valid){
+         songsShown++;
+         displayedSongs[i] = song;
          var name = song.title;
-         if(name.length > 44) name = name.substr(0, 44) + "..."
-         document.getElementById("shelf").innerHTML+= '<div class="slot"> <img draggable="false" src="' + album.art + '" alt="Cover art" title="' + song.fullName + '" class="cover-art"> <span class="title">' + name + '</span> <button class="btn queue" onclick="queue(' + "'" + song.fullName + "'" + ')">Queue</button> <button class="btn play" onclick="play(' + "'" + song.fullName + "'" + ')">Play</button> </div>'
+         if(name.length > 44) name = name.substr(0, 44) + "...";
+         document.getElementById("shelf").innerHTML+= "<div class='slot'> <img draggable='false' src='" + album.art + "' alt='Cover art' title='" + song.fullName + "' class='cover-art'> <span class='title'>" + name + "</span> <button class='btn queue' onclick='queue(" + i + ")'>Queue</button> <button class='btn play' onclick='play(" + i + ")'>Play</button> </div>";
          staffed++;
         }
         i++;
     }
+    window.goneTo = i;
  }
 
  document.getElementById("search").focus();
 
- function queue(song){
+ function queue(id){
+    var song = displayedSongs[id].fullName;
+    song = JSON.stringify(song); 
     socket.emit("queue", song);
  }
 
- function play(song){
+ function play(id){
+    var song = displayedSongs[id].fullName;
+    song = JSON.stringify(song);
     socket.emit("play", song);
  }
 
